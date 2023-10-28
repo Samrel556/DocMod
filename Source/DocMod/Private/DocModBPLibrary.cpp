96b019f37f1cb4feeffed8be5dac06f1d013d132
Samrel <samsigler556@gmail.com>
2023-10-28 14:55:43 -0600

    Creation of Plugin
    Initial setup of Mod

---
+++ Source/DocMod/Private/DocModBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DocModBPLibrary.h"
+#include "DocMod.h"
+
+UDocModBPLibrary::UDocModBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UDocModBPLibrary::DocModSampleFunction(float Param)
+{
+	return -1;
+}
+
